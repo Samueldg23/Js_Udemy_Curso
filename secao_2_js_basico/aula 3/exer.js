eed3be01ec1a9bef2698aeb05914d520a2f77be4
Samuel_Dg <samueldgomes00@gmail.com>
2025-04-25 20:53:18 -0300

    aula 4

---
+++ secao_2_js_basico/aula 3/exer.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Samuel". Estou aprendendo JavaScript às', 19,':',30, 'da noite.');
---
+++ secao_2_js_basico/aula 4/comentarios.js
@@ -0,0 +1,13 @@
+// Comentários em JavaScript
+console.log('Olá mundo!'); // Comentário de uma linha
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4'); 
+//vai ser ignorado
+/**
+ * esse aqui é um bloco de comentários
+ * ele pode ter várias linhas
+ * e vai ser ignorado
+ * e não vai aparecer no console
+ */
