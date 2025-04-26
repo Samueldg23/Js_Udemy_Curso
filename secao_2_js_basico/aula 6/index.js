575c10a3f91a136238d75011e9e4efb83570e1e3
Samuel_Dg <samueldgomes00@gmail.com>
2025-04-25 21:22:20 -0300

    aula 6

---
+++ secao_2_js_basico/aula 6/index.js
@@ -0,0 +1,25 @@
+let nome = 'Otávio';
+
+console.log(nome, 'nasceu em 1984');
+console.log(nome, 'é um programador de 30 anos');
+console.log(nome, 'é casado e tem filhos');
+console.log(nome, 'gosta de futebol e videogame');
+console.log(nome, 'gosta de viajar e ler livros');
+console.log('Em 2010,', nome, 'conheceu sua esposa');
+
+//declarando variáveis
+let idade;
+idade = 30;
+console.log(idade);
+idade = 31;
+console.log(idade);
+
+// não podemos declarar variáveis com o mesmo nome
+/**
+ * não pode declarar com palavras reservadas
+ * não pode começar com números
+ * não pode conter espaços ou traços
+ * precisa ter nomes significativos
+ * utilizar camelCase
+ * Case-sensitive (maiúsculas e minúsculas são diferentes)
+ */
