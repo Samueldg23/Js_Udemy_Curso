4b960bb012e09d43e2a4441eababa7fa0f069391
Samuel_Dg <samueldgomes00@gmail.com>
2025-04-28 16:41:20 -0300

    tipos primitivos em js

---
+++ secao_2_js_basico/aula 10/index.js
@@ -0,0 +1,15 @@
+const nome = 'Luiz';
+const nome1 = "Luiz";
+const nome2 = `Luiz`; // string
+
+const num = 10; //number
+const num1 = 10.52 //number
+let nomeAluno; // underfined -> não aponta pra nada na memória
+let sobrenomeAluno = null; //nulo -> não aponta pra nada na memória mas é diferente do underfined
+
+const boolean = true; // boolean = true ou false (Lógico)
+const aprovado = false;
+
+console.log(typeof aprovado, aprovado);
+
+ 
