0b5ad16b755ca11b3653521d5d03441c19a115c8
Samuel_Dg <samueldgomes00@gmail.com>
2025-04-28 18:33:42 -0300

    navegador exercicios

---
+++ secao_2_js_basico/aula 12/js/script.js
@@ -0,0 +1,6 @@
+let num1 = prompt('Digite um número:')
+let num2 = prompt('Digite outro número:')
+num1 = Number (num1);
+num2 = parseFloat(num2);
+// const resultado = num1 + num2;
+alert(`O resultado da soma foi: ${num1 + num2}`); // dependendo posso colocar a variável dentro do template string
